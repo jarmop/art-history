67ee8bb9dd90084aeb443f4856200e8138e2398e
Jarmo Pihlajaniemi <jarmopih@gmail.com>
2024-07-08 00:38:51 +0300

    Prevent too many image titles error

--- src/wikipedia.tsx
+++ src/wikipedia.tsx
@@ -3,6 +3,7 @@ import { Artist } from './artists'
 
 const apiRoot = 'https://en.wikipedia.org/w/api.php?origin=*&format=json&'
 const imageUrlPrefix = `${apiRoot}action=query&prop=imageinfo&formatversion=2&iiprop=url&iilimit=1&iiurlwidth=300`
+const titlesLimit = 50
 
 type ImageResponseData = {
   query: { pages: [{ imageinfo: [{ thumburl: string }] }] }
@@ -23,6 +24,7 @@ export const useImages = (artistId: Artist['id']) => {
               return imageExtension === 'jpg' || imageExtension === 'jpeg'
             })
             .map((image) => 'File:' + image)
+            .slice(0, titlesLimit)
           const titlesString = '&titles=' + encodeURIComponent(titles.join('|'))
           const imageUrl = imageUrlPrefix + titlesString
 
