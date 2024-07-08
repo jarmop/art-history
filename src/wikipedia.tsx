ac781f447d61689d942406c4ae18a6d98a9e4245
Jarmo Pihlajaniemi <jarmopih@gmail.com>
2024-07-08 10:15:32 +0300

    Optimize for iPhone 15

--- src/wikipedia.tsx
+++ src/wikipedia.tsx
@@ -2,7 +2,7 @@ import { useQuery } from '@tanstack/react-query'
 import { Artist } from './artists'
 
 const apiRoot = 'https://en.wikipedia.org/w/api.php?origin=*&format=json&'
-const imageUrlPrefix = `${apiRoot}action=query&prop=imageinfo&formatversion=2&iiprop=url&iilimit=1&iiurlwidth=300`
+const imageUrlPrefix = `${apiRoot}action=query&prop=imageinfo&formatversion=2&iiprop=url&iilimit=1&iiurlwidth=200`
 const titlesLimit = 50
 
 type ImageResponseData = {
