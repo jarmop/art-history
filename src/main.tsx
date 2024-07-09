2fcc0cdfa838c2a235a6f8332626ddf2c678c934
Jarmo Pihlajaniemi <jarmopih@gmail.com>
2024-07-09 13:00:18 +0300

    Persist React query cache in local storage

--- src/main.tsx
+++ src/main.tsx
@@ -2,15 +2,31 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { QueryClient } from '@tanstack/react-query'
+import { createSyncStoragePersister } from '@tanstack/query-sync-storage-persister'
+import { PersistQueryClientProvider } from '@tanstack/react-query-persist-client'
 
-const queryClient = new QueryClient()
+const MAX_CACHE_AGE = Infinity
 
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      gcTime: MAX_CACHE_AGE,
+    },
+  },
+})
+
+const persister = createSyncStoragePersister({
+  storage: window.localStorage,
+})
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <QueryClientProvider client={queryClient}>
+    <PersistQueryClientProvider
+      client={queryClient}
+      persistOptions={{ persister, maxAge: MAX_CACHE_AGE }}
+    >
       <App />
-    </QueryClientProvider>
+    </PersistQueryClientProvider>
   </React.StrictMode>
 )
